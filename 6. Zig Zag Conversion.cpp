029d94f09a86e9a42bc765141836df336108384e
Your Name <you@example.com>
2022-09-11 15:45:31 -0400

    Add LeetcCode 6

---
+++ 6. Zig Zag Conversion.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+
+        if(numRows == 1) {
+            return s;
+        }
+        
+        string res = "";
+        
+        for(int r = 0; r < numRows; r++) {
+            int increment = 2 * (numRows - 1);
+            for(int i = r; i < s.size(); i += increment) {
+                res += s[i];
+                if(r > 0 && r < numRows - 1 && 
+                   i + increment - 2 * r < s.size()) {
+                    res += s[i + increment - 2 * r];
+                }
+            }
+        }
+        return res;
+    }
+};
+
+
+
+//If first or last row -> numRows - 1 * 2
