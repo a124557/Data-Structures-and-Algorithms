e6fab9cb7dba47dfb2e7f5fba3b7292474625c5f
a124557 <av@alivirani.com>
2023-01-29 22:34:17 -0500

    Update LeetCode 198

--- 198. House Robber.cpp
+++ 198. House Robber.cpp
@@ -3,17 +3,24 @@ class Solution
 public:
     int rob(vector<int> &nums)
     {
-        int rob1 = 0;
-        int rob2 = 0;
-        int temp;
-
-        //[rob1, rob2, n, n+1...]
-        for (int i : nums)
+        if (nums.size() == 0)
+            return 0;
+        if (nums.size() == 1)
+            return nums[0];
+        if (nums.size() == 2)
+            return max(nums[0], nums[1]);
+        vector<int> arr(nums.size());
+        // Initialize arr with max we can rob from first house and second house
+        arr[0] = nums[0];
+        arr[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < nums.size(); i++)
         {
-            temp = max(i + rob1, rob2);
-            rob1 = rob2;
-            rob2 = temp;
+            /* The max we can get from the current house is equal to the
+            max we can get from the previous house and the max from the
+            house before the previous house and the current house*/
+            arr[i] = max(arr[i - 1], arr[i - 2] + nums[i]);
         }
-        return rob2;
+
+        return arr[nums.size() - 1];
     }
 };
